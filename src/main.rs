a9ce6295f01814f7c141081d1aa05f34f992f563
veryslowcode <veryslowcode@gmail.com>
2023-12-20 13:27:18 -0700

    Added event handling.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use crossterm::{
+    event::{self, Event, KeyEventKind, KeyCode},
     execute,
     terminal::{
         disable_raw_mode, enable_raw_mode,
@@ -8,12 +9,60 @@ use crossterm::{
 };
 use ratatui::prelude::*;
 use std::{
-    panic, thread, time,
+    panic, time::Duration,
     io::{stdout, Stdout},
 };
 
 type NolpBackend = CrosstermBackend<Stdout>;
 type NolpTerminal = Terminal<NolpBackend>;
+
+#[derive(Debug, PartialEq)]
+enum Message {
+    Quit,
+    None
+}
+
+fn main() {
+    set_panic_hook();
+
+    init_terminal()
+        .expect("Failed to initialize terminal");
+
+    loop {
+        let msg = handle_event()
+            .expect("Failed to poll events");
+        if msg == Message::Quit {
+            break;
+        }
+    }
+
+    reset_terminal()
+        .expect("Failed to reset terminal");
+}
+
+fn handle_event() -> Result<Message> {
+    let poll_rate = Duration::from_millis(250);
+    if event::poll(poll_rate)? {
+        let event_read = event::read()?;
+        return match event_read {
+            Event::Key(k) => Ok(handle_key_event(k)),
+            _ => Ok(Message::None)
+        }
+    }
+
+    Ok(Message::None)
+}
+
+fn handle_key_event(key: event::KeyEvent) -> Message {
+    if key.kind != KeyEventKind::Press {
+        return Message::None;
+    }
+
+    return match key.code {
+        KeyCode::Char('q') => Message::Quit,
+        _ => Message::None
+    }
+}
 
 fn init_terminal() -> Result<NolpTerminal> {
     enable_raw_mode()?;
@@ -44,16 +93,4 @@ fn set_panic_hook() {
             }
         }
     }));
-}
-
-fn main() {
-    set_panic_hook();
-
-    init_terminal()
-        .expect("Failed to initialize terminal");
-
-    thread::sleep(time::Duration::from_secs(1));
-
-    reset_terminal()
-        .expect("Failed to reset terminal");
 }
