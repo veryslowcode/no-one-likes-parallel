8de5133b5e3d3a7bdb36738c81f382342e509702
veryslowcode <veryslowcode@gmail.com>
2023-12-20 12:59:22 -0700

    Added basic setup for terminal init and reset. Also added panic hook.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use anyhow::Result;
+use crossterm::{
+    execute,
+    terminal::{
+        disable_raw_mode, enable_raw_mode,
+        EnterAlternateScreen, LeaveAlternateScreen
+    }
+};
+use ratatui::prelude::*;
+use std::{
+    panic, thread, time,
+    io::{stdout, Stdout},
+};
+
+type NolpBackend = CrosstermBackend<Stdout>;
+type NolpTerminal = Terminal<NolpBackend>;
+
+fn init_terminal() -> Result<NolpTerminal> {
+    enable_raw_mode()?;
+    execute!(stdout(), EnterAlternateScreen)?;
+    let backend = NolpBackend::new(stdout());
+    let terminal = NolpTerminal::new(backend)?;
+    Ok(terminal)
+}
+
+fn reset_terminal() -> Result<()> {
+    execute!(stdout(), LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
+
+fn set_panic_hook() {
+    let hook = panic::take_hook();
+    panic::set_hook(Box::new(move |info| {
+        reset_terminal().unwrap();
+        eprintln!("An unexpected error occured");
+        if cfg!(debug_assertions) {
+            hook(info);
+        } else {
+            let err_msg = info.payload().downcast_ref::<&str>();
+            match err_msg {
+               Some(msg) => eprintln!("{:?}", msg),
+               None => {}
+            }
+        }
+    }));
+}
+
 fn main() {
-    println!("Working...?");
+    set_panic_hook();
+
+    init_terminal()
+        .expect("Failed to initialize terminal");
+
+    thread::sleep(time::Duration::from_secs(1));
+
+    reset_terminal()
+        .expect("Failed to reset terminal");
 }
