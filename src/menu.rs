ab886c3d2ca9b7ac74612c07bb08e2b552d48e82
veryslowcode <veryslowcode@gmail.com>
2023-12-23 13:14:44 -0700

    Updated scrollbar style and resolved issues affecting menu layout.

--- src/menu.rs
+++ src/menu.rs
@@ -119,16 +119,15 @@ impl Tea for MenuModel {
     fn view(&mut self, frame: &mut Frame) {
         let (bounds, layout) = menu_layout(frame.size());
 
-        let top_margin = 4;
+        let input_gap = 8;
         let input_width = 14;
-        let input_width_gap = 4;
-        let input_line_count = 3;
+        let min_width = (input_width * 2) + input_gap;
 
         let mut split = true;
-        let mut min_height = ((self.inputs.len() * input_line_count) + top_margin) / 2;
-        let min_width = (input_width * 2) + input_width_gap;
+        let mut min_height = (self.inputs.len() / 2) * 3 + 2;
+
         if bounds.width < min_width {
-            min_height = min_height * 2;
+            min_height = self.inputs.len() * 3 + 2;
             split = false;
         }
 
@@ -139,28 +138,41 @@ impl Tea for MenuModel {
 
         frame.render_widget(title, layout[0]);
 
-        let elements = input_elements(&mut self.inputs, self.selected.into(), split);
-        let menu = Paragraph::new(elements).scroll((0, 0));
+        let elements = input_elements(
+            &mut self.inputs,
+            self.selected.into(),
+            split,
+            input_width.into(),
+            input_gap.into(),
+        );
+        let menu = Paragraph::new(elements)
+            .scroll((0, 0))
+            .alignment(Alignment::Center);
 
         frame.render_widget(menu, layout[2]);
 
-        if usize::from(bounds.height) < min_height {
+        if usize::from(bounds.height) <= min_height {
             let scrollbar = Scrollbar::default()
-                .begin_symbol(Some("↑"))
-                .track_symbol(Some("-"))
-                .thumb_symbol("░")
-                .end_symbol(Some("↓"))
-                .orientation(ScrollbarOrientation::VerticalRight);
+                .orientation(ScrollbarOrientation::VerticalRight)
+                .track_symbol(None)
+                .thumb_symbol("");
 
             frame.render_stateful_widget(scrollbar, layout[2], &mut self.scroll);
         }
     }
 }
 
-fn input_elements(inputs: &mut Vec<MenuInput>, _selected: usize, split: bool) -> Vec<Line> {
+fn input_elements(
+    inputs: &mut Vec<MenuInput>,
+    _selected: usize,
+    split: bool,
+    width: usize,
+    gap: usize,
+) -> Vec<Line> {
+    let gap_fmt = " ".repeat(gap);
+    let underline_fmt = "▔".repeat(width);
+
     let mut elements = Vec::new();
-    // let mut underline = Line::from(format!("{:▔>14}", ""));
-    // let selected_style = Style::default().fg(Color::LightBlue);
 
     let iterations = if split {
         inputs.len() / 2
@@ -173,26 +185,26 @@ fn input_elements(inputs: &mut Vec<MenuInput>, _selected: usize, split: bool) ->
         let input: Line;
         let underline: Line;
 
-        let underline_format = format!("{:▔>14}", "");
-
         if split {
             title = Line::from(format!(
-                "{: <14}{: <8}{: <14}",
+                "{: <w$}{}{: <w$}",
                 inputs[i].title.to_string(),
-                "",
-                inputs[i + 3].title.to_string()
+                &gap_fmt,
+                inputs[i + 3].title.to_string(),
+                w = width
             ));
             input = Line::from(format!(
-                "{: <14}{: <8}{: <14}",
+                "{: <w$}{}{: <w$}",
                 input_text(&mut inputs[i]),
-                "",
-                input_text(&mut inputs[i + 3])
+                &gap_fmt,
+                input_text(&mut inputs[i + 3]),
+                w = width
             ));
-            underline = Line::from(format!("{}{: <8}{}", underline_format, "", underline_format));
+            underline = Line::from(format!("{}{}{}", underline_fmt, gap_fmt, underline_fmt));
         } else {
             title = Line::from(inputs[i].title.to_string());
-            input = Line::from(format!("{}", input_text(&mut inputs[i])));
-            underline = Line::from(underline_format);
+            input = Line::from(input_text(&mut inputs[i]));
+            underline = Line::from(underline_fmt.clone());
         }
 
         elements.push(title);
@@ -217,9 +229,9 @@ fn menu_layout(fsize: Rect) -> (Rect, Rc<[Rect]>) {
     let layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
-            Constraint::Length(4),
             Constraint::Length(1),
-            Constraint::Min(1),
+            Constraint::Length(2),
+            Constraint::Min(3),
         ])
         .split(bounds);
 
